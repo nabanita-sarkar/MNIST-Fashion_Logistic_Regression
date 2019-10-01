22cfbc2e64f917a10ea47469f987f3fc27cc7792
Nabanita Sarkar <36895154+nabanita-sarkar@users.noreply.github.com>
2019-10-01 21:32:29 +0530

    Updated with proper comments

--- MNIST-Fashion-Complete.py
+++ MNIST-Fashion-Complete.py
@@ -2,59 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Load training and testing csv datasets
 train_file = pd.read_csv("fashion-mnist_train.csv")
 test_file = pd.read_csv("fashion-mnist_test.csv")
 
-#train_file.head()
-
-#print(train_file.shape)
-#print(test_file.shape)
-
+#Deleting all except the first label column from the training datasets
 y_train = train_file.values
 y_train=np.delete(y_train,slice(1,785),axis=1)
 y_train=y_train.ravel()
 
+#Deleting all except the first label column from the testing datasets
 y_test = test_file.values
 y_test=np.delete(y_test,slice(1,785),axis=1)
 y_test=y_test.ravel()
 
-#print(y_train.shape)
-#print(y_test.shape)
-
-#y_train[0:5]
-
+#Dropping label column from traing and testing datasets
 train_file.drop('label',axis=1, inplace=True)
 test_file.drop('label',axis=1, inplace=True)
 
-#train_file.head()
-
+#Converting training dataset from pandas dataframe to numpy array
 train_file_arr = train_file.values
 
+#Showing first 5 elements of the dataset
 plt.figure(figsize=(20,4))
 for index, (image, label) in enumerate(zip(train_file_arr[0:5], y_train[0:5])):
     plt.subplot(1, 5, index+1)
     plt.imshow(np.reshape(image,(28,28)), cmap=plt.cm.gray)
     plt.title('Training: %i\n' %label, fontsize = 20)
-
 plt.show()
 
+#importing Logistic Regression Model
 from sklearn.linear_model import LogisticRegression
 logisticRegr = LogisticRegression(solver='lbfgs')
 logisticRegr.fit(train_file,y_train)
 
 #y_test[0:10]
-
 #test_file_arr.shape
-
 #test_file_arr = test_file.values
-
-
 #logisticRegr.predict(test_file_arr[0].reshape(1,-1))
-
 #logisticRegr.predict(test_file[0:10])
 
-predictions=logisticRegr.predict(test_file)
-
+#Printing the accuracy
+predictions = logisticRegr.predict(test_file)
 score = logisticRegr.score(test_file, y_test)
 print(score)
 
@@ -63,7 +52,7 @@ mis_classified_index = []
 for label, predict in zip(y_test,predictions):
     if label!=predict:
         mis_classified_index.append(index)
-    index+=1
+    index+ = 1
 
 plt.figure(figsize=(20,4))
 for plot_index, bad_index in enumerate(mis_classified_index[0:5]):
@@ -72,10 +61,3 @@ for plot_index, bad_index in enumerate(mis_classified_index[0:5]):
     plt.title('Predicted: {}, Actual: {}'.format(predictions[bad_index], y_test[bad_index]), fontsize=20)
     
 y_test[0:10]
-
-
-
-
-
-
-
